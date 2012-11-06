c9b9d425878c25affe5fa1b3106aa3ee9cc17c66
Julian Duniec <julian.duniec@gmail.com>
2012-11-06 17:06:46 +0100

    Stuffzor

--- jdstatistics.erl
+++ jdstatistics.erl
@@ -3,7 +3,7 @@
 -export([
 		mean/1,
 		meansquarederror/2, 
-		meansquarederror/2]).
+		squarederror/2]).
 
 %% Calculates the mean of the supplied list
 %% http://en.wikipedia.org/wiki/Arithmetic_mean
@@ -14,11 +14,11 @@ mean(List) ->
 %% http://en.wikipedia.org/wiki/Mean_squared_error
 meansquarederror(Predictions, True) when length(Predictions) =/= length(True) -> erlang:error("Length of predictions must equal length of True values.");
 meansquarederror(Predictions, True) -> 
-	1/length(Predictions) * mse_sumerrors(Predictions, True, 0).
+	1/length(Predictions) * meansquarederror_sumerrors(Predictions, True, 0).
 %% Helper for meansquarederror -> sums the squarederror for both lists
 meansquarederror_sumerrors(P, T, Sum) when length(P) =:= 0 -> Sum;
 meansquarederror_sumerrors(P, T, Sum) ->
-	Sum + squarederror(hd(P), hd(T)) + mse_sumerrors(tl(P), tl(T), Sum).
+	Sum + squarederror(hd(P), hd(T)) + meansquarederror_sumerrors(tl(P), tl(T), Sum).
 
 %% Calculates the squared error between X and Y
 squarederror(X,Y) -> math:pow((X-Y),2).
