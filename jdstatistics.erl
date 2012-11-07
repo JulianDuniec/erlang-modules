e0026ab4218ef083f7392879763039c1808204d8
Julian Duniec <julian.duniec@gmail.com>
2012-11-07 11:22:16 +0100

    Added pearson coefficient

--- jdstatistics.erl
+++ jdstatistics.erl
@@ -2,8 +2,9 @@
 -module(jdstatistics).
 -export([
 		mean/1,
-		meansquarederror/2, 
-		squarederror/2]).
+		mean_squared_error/2, 
+		squared_error/2,
+		pearson_correlation/2]).
 
 %% Calculates the mean of the supplied list
 %% http://en.wikipedia.org/wiki/Arithmetic_mean
@@ -15,22 +16,40 @@ mean(List) ->
 %% http://en.wikipedia.org/wiki/Mean_squared_error
 
 %% Guard for different lengths
-meansquarederror(Predictions, True) 
-	when length(Predictions) =/= length(True) -> 
-		erlang:error("Length of predictions must equal length of True values.");
+mean_squared_error(A, B) 
+	when length(A) =/= length(B) -> 
+		erlang:error("Length of A must equal length of B");
 
 %% Guard for zero-length lists
-meansquarederror(Predictions, True)
-	when (length(Predictions) =:= 0) or (length(True) =:= 0) ->
-		erlang:error("Length of predictions or true values cannot be zero");
+mean_squared_error(A, B)
+	when (length(A) =:= 0) or (length(B) =:= 0) ->
+		erlang:error("Length of A or B cannot be zero");
 
-meansquarederror(Predictions, True) -> 
-	1/length(Predictions) * meansquarederror_sumerrors(Predictions, True, 0).
-
-%% Helper for meansquarederror -> sums the squarederror for both lists
-meansquarederror_sumerrors(P, T, Sum) when (length(P) =:= 0) and (length(T) =:= 0) -> Sum;
-meansquarederror_sumerrors(P, T, Sum) ->
-	Sum + squarederror(hd(P), hd(T)) + meansquarederror_sumerrors(tl(P), tl(T), Sum).
+mean_squared_error(A, B) -> 
+	1/length(A) * lists:sum([squared_error(X,Y) || {X,Y} <- lists:zip(A,B)]).
 
 %% Calculates the squared error between X and Y
-squarederror(X,Y) -> math:pow((X-Y),2).
+squared_error(X,Y) -> math:pow((X-Y),2).
+
+
+%% Calculates the Pearson correlation coefficient between A and B
+%% http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
+
+pearson_correlation(A,B) 
+	when length(A) =/= length(B) ->
+		erlang:error("Lengths of lists A and B must equal");
+
+pearson_correlation(A,B)
+	when (length(A) =:= 0) or (length(B) =:= 0) ->
+		erlang:error("Length of A or B cannot be zero");
+
+pearson_correlation(A,B) ->
+	SumXY = lists:sum([X*Y || {X,Y} <- lists:zip(A,B)]),
+	SumX = lists:sum(A),
+	SumY = lists:sum(B),
+	SumXX = lists:sum([L*L || L <- A]),
+	SumYY = lists:sum([L*L || L <- B]),
+	N = length(A),
+	Numer = (N*SumXY) - (SumX * SumY),
+	Denom = math:sqrt(((N*SumXX)-(SumX*SumX)) * ((N*SumYY)-(SumY*SumY))),
+	Numer/Denom.
